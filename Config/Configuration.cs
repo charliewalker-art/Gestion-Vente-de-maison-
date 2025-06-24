a7910e8ab4657e64b00c28e02da1bb6422bc63b0
charliewalker-art <charliewaltert0206@gmail.com>
2025-06-24 11:27:48 +0300

    initialisation du projet

---
+++ Config/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using GestionMaison;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VenteMaison
+{
+    class Configuration
+    {
+        private static Database _instance;
+
+        // Méthode pour récupérer la chaîne de connexion
+        public static string GetConnexion()
+        {
+            if (_instance == null)
+            {
+                // Initialisation une seule fois
+                _instance = new Database("localhost", "gestion_maison", "root", "");
+                _instance.OpenConnection();
+            }
+
+            return _instance.valeurconnexion();
+        }
+
+        // Si besoin d’accéder directement à l’objet Database
+        public static Database GetDatabaseInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Database("localhost", "gestion_maison", "root", "");
+                _instance.OpenConnection();
+            }
+
+            return _instance;
+        }
+
+
+    }
+}
